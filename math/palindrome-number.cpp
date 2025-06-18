bb8d3b62496860251159963b1bdcd2aeff0b2384
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-19 00:48:25 +0530

    LC-palindrome-number [Runtime: 24 ms; Memory: 14.4 MB]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long temp = x;
-        long num = 0;
-        while(temp>0){
-            num = num*10 + temp % 10;
-            temp = temp / 10;
+        vector<int> y;
+        if (x < 0) {
+            return false;
         }
-        if(x == num){return true;}
-        return false;
+        while(x>0){
+            y.push_back(x%10);
+            x = x/10;
+        }
+        vector<int> z = y;
+        for (int i = 0; i < y.size() / 2; i++) {
+            y[i] = y[y.size() - i - 1];
+        }
+
+        if (y == z) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
