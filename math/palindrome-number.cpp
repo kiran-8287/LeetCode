deeb0954f34b9df07ed8b93e605ed35cf423d401
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-19 00:30:41 +0530

    LC-palindrome-number [Runtime: 8 ms; Memory: 8.6 MB]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         int temp = x;
-        int num = 0;
+        long num = 0;
         while(temp>0){
             num = num*10 + temp % 10;
             temp = temp / 10;
