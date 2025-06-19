10dc4c38f22fcc5eb8f9f1d46c39ba59d1ba7339
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-19 23:03:42 +0530

    LC-sqrtx [Runtime: 0 ms; Memory: 8.6 MB]

--- binary-search/sqrtx.cpp
+++ binary-search/sqrtx.cpp
@@ -7,7 +7,7 @@ public:
         if (x == 1) {
             return 1;
         }
-        while (left < right) {
+        while (left <= right) {
             long mid = left + (right - left) / 2;
             if (mid * mid <= x) {
                 num = mid;
