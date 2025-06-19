1a55a04f16361af61a2acc223dca258b0971699d
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-19 17:49:56 +0530

    LC-transform-array-by-parity [Runtime: 0 ms; Memory: 28.2 MB]

--- counting/transform-array-by-parity.cpp
+++ counting/transform-array-by-parity.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     vector<int> transformArray(vector<int>& nums) {
+        int eve=0;
+        int odd = 0;
         for(int i = 0; i < nums.size(); i++){
             if(nums[i]%2 == 0){
-                nums[i] = 0;
+                eve++;
             }
             else{
-                nums[i] = 1;
+                odd++;
             }
         }
-        sort(nums.begin(), nums.end());
+        for(int i = 0; i < eve; i++){
+            nums[i] = 0;
+        }
+        for(int i = eve; i < eve + odd; i++){
+            nums[i] = 1;
+        }
         return nums;
     }
 };
