94b6cc8fa341fe98f3535065163605cbcda01771
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-19 22:50:14 +0530

    LC-sqrtx [Runtime: N/A; Memory: N/A]

---
+++ math/sqrtx.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int max = 0;
+        for(int i = 1; i < x ; i++){
+            if (i*i <= x && i > max){
+                max = i;
+            }
+        }
+        return max;
+    }
+};
