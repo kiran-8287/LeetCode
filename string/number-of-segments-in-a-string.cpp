14be6d12d6e528df0e15383b7b533b71c25f7348
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-23 00:35:38 +0530

    LC-number-of-segments-in-a-string [Runtime: N/A; Memory: N/A]

--- string/number-of-segments-in-a-string.cpp
+++ string/number-of-segments-in-a-string.cpp
@@ -3,11 +3,11 @@ public:
     int countSegments(string s) {
         if(s.empty()){return 0;}
         int count = 0;
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 1; i < s.length()-1; i++){
             if(s[i] == ' ' && (isalpha(s[i-1]) || isalpha(s[i+1]))){
                 count += 1;
             }
         }
-        return count +1;
+        return count+1;
     }
 };
