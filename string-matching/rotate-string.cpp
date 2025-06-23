c1ae95622712505fc77d19045a7706cf632c8d57
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-24 00:15:16 +0530

    LC-rotate-string [Runtime: 0 ms; Memory: 8.2 MB]

--- string-matching/rotate-string.cpp
+++ string-matching/rotate-string.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-    if(s.length()!=goal.length()){return false;}
+        if(s.length()!=goal.length()){return false;}
+        string temp = goal + goal ;
+        return (!(temp.find(s) == string::npos));
     }
 };
