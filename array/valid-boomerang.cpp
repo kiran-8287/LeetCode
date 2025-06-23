2c73230eedf87eb7375092ab4dba6cfc2ab45606
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-23 23:24:06 +0530

    LC-valid-boomerang [Runtime: N/A; Memory: N/A]

---
+++ array/valid-boomerang.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        int x = (points[0][1] - points[1][1])/(points[0][0] - points[1][0]);
+        int y = (points[0][1] - points[2][1])/(points[0][0] - points[2][0]);
+        if(x!=y){return true;}
+        return false;
+    }
+};
