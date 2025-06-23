bb87ab8ecd497d1c19a0fafe27887e84a70fd7cc
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-24 01:06:51 +0530

    LC-goat-latin [Runtime: 0 ms; Memory: 9.3 MB]

--- string/goat-latin.cpp
+++ string/goat-latin.cpp
@@ -20,7 +20,7 @@ public:
 
         for(int i = 0; i < words.size(); i++){
             string as(i+1, 'a');
-            if(words[i][0] == 'a'||words[i][0] == 'e'||words[i][0] == 'i'||words[i][0] == 'o'||words[i][0] == 'u'){
+            if(words[i][0] == 'a'||words[i][0] == 'e'||words[i][0] == 'i'||words[i][0] == 'o'||words[i][0] == 'u'||words[i][0] == 'A'||words[i][0] == 'E'||words[i][0] == 'I'||words[i][0] == 'O'||words[i][0] == 'U'){
                 words[i] += "ma" + as;
             }
             else{
