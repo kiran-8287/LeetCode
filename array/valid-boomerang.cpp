41bcd4a0a0d4c183628b787b3b002b0e285cc185
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-23 23:37:45 +0530

    LC-valid-boomerang [Runtime: 0 ms; Memory: 12.6 MB]

--- array/valid-boomerang.cpp
+++ array/valid-boomerang.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool isBoomerang(vector<vector<int>>& points) {
-        int x = (points[0][1] - points[1][1])/(points[0][0] - points[1][0]);
-        int y = (points[0][1] - points[2][1])/(points[0][0] - points[2][0]);
-        if(x!=y){return true;}
+        int y1 = points[0][1] - points[1][1];
+        int x1 = points[0][0] - points[1][0];
+        int y2 = points[0][1] - points[2][1];
+        int x2 = points[0][0] - points[2][0];
+        if(y1*x2 - y2*x1 != 0){return true;}
         return false;
     }
 };
