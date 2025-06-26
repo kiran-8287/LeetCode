a5d216f3949eaf4e1fc117d00a188f62153bc73f
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-27 00:31:29 +0530

    LC-generate-a-string-with-characters-that-have-odd-counts [Runtime: 0 ms; Memory: 8.2 MB]

---
+++ string/generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        string s = "";
+        for(int i = 0; i < n; i++){
+            s += 'a';
+        }
+        if(n%2==0){s[n-1] = 'b';}
+        return s;
+    }
+};
