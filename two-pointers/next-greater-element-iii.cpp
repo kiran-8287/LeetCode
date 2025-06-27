be77032aa690f2b1d95d5d7b9ead1e7ee0cdbe86
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-28 00:41:40 +0530

    LC-next-greater-element-iii [Runtime: N/A; Memory: N/A]

--- two-pointers/next-greater-element-iii.cpp
+++ two-pointers/next-greater-element-iii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int nextGreaterElement(int n) {
+        if (n < 10) {return -1;}
         int m = n;
         vector<int> nums;
         while(n > 0){
@@ -16,7 +17,7 @@ public:
         for(int i = 0; i < nums.size(); i++){
             val = val*10 + nums[i];
         }
-        
+
         if(val > m){return val;}
         return -1;
     }
