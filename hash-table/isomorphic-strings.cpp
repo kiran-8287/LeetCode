38853517946dc6c90f5ce989aae7ba245516cf14
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-30 01:55:35 +0530

    LC-isomorphic-strings [Runtime: 0 ms; Memory: 9.4 MB]

--- hash-table/isomorphic-strings.cpp
+++ hash-table/isomorphic-strings.cpp
@@ -3,11 +3,17 @@ public:
     bool isIsomorphic(string s, string t) {
         if(s.length() != t.length()){return false;}
         unordered_map<char , char> st;
+        unordered_map<char , char> ts;
+
         for(int i = 0; i < s.length(); i++){
             if(st[s[i]] != 0 && st[s[i]] != t[i]){
                 return false;
             }
+            if(ts[t[i]] != 0 && ts[t[i]] != s[i]){
+                return false;
+            }
             st[s[i]] = t[i];
+            ts[t[i]] = s[i];
         }
         return true;
     }
