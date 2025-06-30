c117a23f6a2d85ef2b8e8df456cf059f85b505fe
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-30 17:20:27 +0530

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

---
+++ sorting/contains-duplicate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,bool> b;
+        for(int i=0;i<nums.size();i++){
+            if(b[nums[i]]){
+               return true;
+               break; 
+            }
+            b[nums[i]]=true;
+
+        }
+        return false;;
+    }
+};
+    }
+};
