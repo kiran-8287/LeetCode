15a1a71f0c69ee8ce6fcdf0f76f1be2c9b49743e
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-30 17:19:11 +0530

    LC-find-all-numbers-disappeared-in-an-array [Runtime: 79 ms; Memory: 78 MB]

--- hash-table/find-all-numbers-disappeared-in-an-array.cpp
+++ hash-table/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
+        unordered_map<int, bool> map;
         vector<int> out;
-        for(int i = 1; i < nums.size()+1; i++){
-            if (find(nums.begin(), nums.end(), i) == nums.end()){
-                out.push_back(i);
-                }
+
+        for (int i = 0; i < nums.size(); i++) {
+            map[nums[i]] = true;
         }
+
+        for (int i = 1; i < nums.size()+1; i++) {
+            if (map[i] == false) {
+                out.push_back(i);
+            }
+        }
+
         return out;
     }
 };
