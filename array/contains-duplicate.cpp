67e9633c7768ba380074cdfdeab55ca47763b09d
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-30 17:23:40 +0530

    LC-contains-duplicate [Runtime: 62 ms; Memory: 90.9 MB]

--- array/contains-duplicate.cpp
+++ array/contains-duplicate.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,bool> b;
         for(int i=0;i<nums.size();i++){
-            if(b[nums[i]]){
+            if(b[nums[i]] == true){
                return true;
-               break; 
             }
-            b[nums[i]]=true;
-
+            b[nums[i]] = true;
         }
         return false;;
     }
