ac04d51d6aee9b63a02cfa668ad469f151150c9c
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-06-30 20:58:21 +0530

    LC-ugly-number [Runtime: N/A; Memory: N/A]

--- math/ugly-number.cpp
+++ math/ugly-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isUgly(int n) {
+        if(n < 0){return false;}
         int m = n;
-        if(m > 0){
         while(m > 1 ){
             if(m%2==0){
                 m = m/2;
@@ -16,23 +16,6 @@ public:
             else{
                 return false;
             }
-        }
-        }
-        else{
-            while(m != -1 ){
-            if(m%2==0){
-                m = m/2;
-            }
-            else if(m%3 == 0){
-                m = m/3;
-            }
-            else if(m%5 == 0){
-                m = m/5;
-            }
-            else{
-                return false;
-            }
-        }
         }
         return true;
     }
