57ed5f7e6875e42842ef6b3c865572367737cbcc
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-01 00:42:25 +0530

    LC-destination-city [Runtime: 1 ms; Memory: 15.4 MB]

--- hash-table/destination-city.cpp
+++ hash-table/destination-city.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        unordered_map<string, string> map;
-        string out;
-        for(int i = 0; i < paths.size(); i++){
-            map[paths[i][0]] = paths[i][1];
-            map[paths[i][1]] = "";
+        string s="";
+        unordered_map <string,string> map;
+        for(int i=0;i<paths.size();i++){
+            map[paths[i][0]]=paths[i][1];
         }
-        for(int i = 0; i < paths.size(); i++){
-            if(map[paths[i][1]] == ""){
-                out = paths[i][1];
+        for(int j=0;j<paths.size();j++){
+            if(map[paths[j][1]]==""&&map[paths[j][0]]!=""){
+                s= paths[j][1];
             }
         }
-        return out;
+        return s; 
     }
 };
