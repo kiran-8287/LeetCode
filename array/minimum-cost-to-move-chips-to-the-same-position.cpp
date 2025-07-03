480f95ae2b242b9199eca40e3f9fa5ffc5441e20
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-03 21:27:52 +0530

    LC-minimum-cost-to-move-chips-to-the-same-position [Runtime: 0 ms; Memory: 10 MB]

--- array/minimum-cost-to-move-chips-to-the-same-position.cpp
+++ array/minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int minCostToMoveChips(vector<int>& position) {
-        int max_count = 0;
-        int best_pos = position[0];
-        int cost = 0;
+        int pos_even = 0;
+        int pos_odd = 0;
         for(int i = 0; i < position.size(); i++){
-            int count = 0;
-            for(int j = i; j < position.size(); j++){
-                if(position[i] == position[j]){
-                    count++;
-                }
+            if(position[i] % 2 == 0){
+                pos_even++;
             }
-            if(count>max_count){
-                max_count = count;
-                best_pos = position[i];
+            else{
+                pos_odd++;
             }
         }
-        for(int i =0; i < position.size(); i++){
-            if(position[i] != best_pos){
-                if(abs(position[i] - best_pos)%2 == 1){
-                    cost++;
-                }
-            }
+        if(pos_odd > pos_even){
+            return pos_even;
         }
-        return cost;
+        return pos_odd;
     }
 };
