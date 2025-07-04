2106e78025d21fba1db74d13a74218b03531070a
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-04 16:56:26 +0530

    LC-hand-of-straights [Runtime: N/A; Memory: N/A]

--- array/hand-of-straights.cpp
+++ array/hand-of-straights.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-       if(hand.size()%groupSize != 0){return false;}
-       return true;
+        if(hand.size()%groupSize != 0){return false;}
+        sort(hand.begin(), hand.end());
+        for(int i = 0; i < hand.size(); i++ ){
+            
+        }
+
+        return true;
     }
 };
