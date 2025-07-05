6d940cb7260f087ef6084b316e48f392a94a0460
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-05 22:18:15 +0530

    LC-power-of-two [Runtime: N/A; Memory: N/A]

--- math/power-of-two.cpp
+++ math/power-of-two.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n%2==1 && n!=1){return false;}
-        else{
-            return true;
+        else if (n%2 == 0){
+            if(isPowerOfTwo(n/2) == true){return true;}
         }
-        return isPowerOfTwo(n/2);
+        return true;
+        
     }
 };
