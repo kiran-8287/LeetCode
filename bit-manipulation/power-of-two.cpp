c8c047b33cd0d16f4d25291426c11b4a14104290
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-05 22:20:42 +0530

    LC-power-of-two [Runtime: N/A; Memory: N/A]

--- bit-manipulation/power-of-two.cpp
+++ bit-manipulation/power-of-two.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n%2==1 && n!=1){return false;}
-        else if (n%2 == 0){
-            if(isPowerOfTwo(n/2) == true){return true;}
+        while(n>1){
+            if(n%2==1){
+                return false;
+            }
+            n = n/2;
         }
         return true;
-        
     }
 };
