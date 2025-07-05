c1a61ec79bfab514eb951c80b667dba3f23aa752
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-05 22:21:19 +0530

    LC-power-of-two [Runtime: 0 ms; Memory: 7.8 MB]

--- recursion/power-of-two.cpp
+++ recursion/power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n == 0){return false;}
+        if(n <= 0){return false;}
         while(n>1){
             if(n%2==1){
                 return false;
