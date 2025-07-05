10b6dce5a49b1c34cb2cf208e13f8c22c69b98ba
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-06 00:22:07 +0530

    LC-divide-array-into-equal-pairs [Runtime: 8 ms; Memory: 16.8 MB]

--- hash-table/divide-array-into-equal-pairs.cpp
+++ hash-table/divide-array-into-equal-pairs.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        unordered_map<int, int> map;
-        for(int i = 0; i < nums.size(); i++){
-            if(map[nums[i]] == 0){
-                map[nums[i]] = 1;
-            }
-            else{
-                map[nums[i]]++;
-            }
-        }
-        for(const auto& [key, value] : map){
-            if(value%2!=0){
-                return false;
+        vector<int> unq;
+        for (int i = 0; i < nums.size(); i++) {
+            int count = 0;
+            if (find(unq.begin(), unq.end(), nums[i]) == unq.end()) {
+                for (int j = i; j < nums.size(); j++) {
+                    if(nums[i] == nums[j]){
+                        count++;
+                    }
+                }
+                if(count%2 != 0){return false;}
+                unq.push_back(nums[i]);
             }
         }
         return true;
