456d2f8854856fa5020822e2724d8784d8331430
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-06 23:39:29 +0530

    LC-slowest-key [Runtime: 0 ms; Memory: 14.8 MB]

--- array/slowest-key.cpp
+++ array/slowest-key.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
     char slowestKey(vector<int>& releaseTimes, string keysPressed) {
-        unordered_map<char, int> map;
-        vector<char> unq;
-        for (int i = 0; i < keysPressed.length(); i++) {
-            if (map[keysPressed[i]] == 0) {
-                unq.push_back(keysPressed[i]);
-            }
-        }
-        map[keysPressed[0]] = releaseTimes[0];
-        for (int i = 1; i < releaseTimes.size(); i++) {
-            if (map[keysPressed[i]] == 0) {
-                map[keysPressed[i]] = releaseTimes[i] - releaseTimes[i - 1];
-            } else {
-                map[keysPressed[i]] =
-                    map[keysPressed[i]] + releaseTimes[i] - releaseTimes[i - 1];
-            }
-        }
-        int maxValue = map[keysPressed[0]];
+        int max = releaseTimes[0];
         char out = keysPressed[0];
 
-        for (const auto& [ch, val] : map) {
-            if (val > maxValue) {
-                maxValue = val;
-                out = ch;
+        for (int i = 1; i < releaseTimes.size(); i++) {
+            int temp = releaseTimes[i] - releaseTimes[i - 1];
+            if (temp > max || (temp == max && keysPressed[i] > out)) {
+                max = temp;
+                out = keysPressed[i];
             }
         }
         return out;
