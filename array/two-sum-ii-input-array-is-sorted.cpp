1d291d92187d5265d568aef3142b8d8777c60850
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-07 18:45:24 +0530

    LC-two-sum-ii-input-array-is-sorted [Runtime: 0 ms; Memory: 19.6 MB]

--- array/two-sum-ii-input-array-is-sorted.cpp
+++ array/two-sum-ii-input-array-is-sorted.cpp
@@ -6,15 +6,15 @@ public:
         int r = numbers.size() - 1;
         while(l<r){
             if(numbers[l] + numbers[r] == target ){
-                out.push_back(l);
-                out.push_back(r);
+                out.push_back(l+1);
+                out.push_back(r+1);
                 break;
             }
             else if(numbers[l] + numbers[r] < target ){
                 l++;
             }
             else if(numbers[l] + numbers[r] > target){
-                r++;
+                r--;
             }
         }
         return out;
