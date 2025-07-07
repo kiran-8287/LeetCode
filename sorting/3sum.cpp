bcb412b81a0896c27df7e0681db0ddba13a10ac8
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-07 19:52:00 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.cpp
+++ sorting/3sum.cpp
@@ -17,6 +17,12 @@ public:
                 int s = nums[i] + nums[l] + nums[r];
                 if (s == 0) {
                     out.push_back({nums[i], nums[l], nums[r]});
+                    while(l < r && nums[l] == nums[l+1]){
+                        l++;
+                    }
+                    while(l < r && nums[r] == nums[r-1]){
+                        r--;
+                    }
                     l++;
                     r--;
                 }
