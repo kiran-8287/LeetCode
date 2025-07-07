69bf01045d88ab155ae0979d8382de2f8620b4a8
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-07 16:23:03 +0530

    LC-group-anagrams [Runtime: N/A; Memory: N/A]

---
+++ sorting/group-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> out;
+        unordered_map<string, int> map;
+        for(int i = 0; i < strs.size(); i++){
+            vector<string> arr;
+            string temp1 = strs[i];
+            sort(temp1.begin(), temp1.end());
+            for(int j = i; j < strs.size(); j++){
+                string temp2 = strs[j];
+                sort(temp2.begin(), temp2.end());
+                if(temp1 == temp2 && map[strs[j]] == 0){
+                    arr.push_back(strs[j]);
+                    map[strs[j]] = 1;
+                }
+            }
+            if(arr.empty() == false){out.push_back(arr);}
+        }
+        sort(out.begin(), out.end());
+        return out;
+    }
+};
