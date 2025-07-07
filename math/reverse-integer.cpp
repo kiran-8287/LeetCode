40a10744cb3f61dd5ba8abdca42135a8f3403aac
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-08 00:19:18 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.cpp
+++ math/reverse-integer.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int reverse(int x) {
-        int out = 0;
-        int temp = abs(x);
-        int n = 0;
+        long temp = abs(x);
+        long n = 0;
         while(temp > 0){
             n = n*10 + temp%10;
             temp = temp/10;
