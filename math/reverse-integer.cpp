8a39f7c61d4e642a3436dc69c81bac0b0e8d50a0
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-08 00:20:52 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.cpp
+++ math/reverse-integer.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int reverse(int x) {
-        long temp = abs(x);
-        long n = 0;
+        if(x > pow(2, 31) - 1 || x < -pow(2, 31)){return 0;}
+        int temp = abs(x);
+        int n = 0;
         while(temp > 0){
             n = n*10 + temp%10;
             temp = temp/10;
