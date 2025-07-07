ed03b1a7470ed60cd3fc088d77165b0ed19885f3
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-08 01:10:26 +0530

    LC-find-minimum-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/find-minimum-in-rotated-sorted-array.cpp
+++ array/find-minimum-in-rotated-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int findMin(vector<int>& nums) {
         int l = 0;
         int r = nums.size() - 1;
-        while(l < r){
+        while(l <= r){
             int m = (r+l)/2;
             if(nums[m] > nums[r]){
                 int temp = nums[m];
