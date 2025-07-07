2f026cd3049eeef5311bb4a4184b79c10d4c94a0
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-07 19:50:27 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.cpp
+++ sorting/3sum.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> out;
+        sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size(); i++) {
-            int left = i + 1;
-            int right = nums.size() - 1;
+            int l = i + 1;
+            int r = nums.size() - 1;
             if (i >= 1) {
-                if (nums[i] == nums[i - 1]) {
+                if(nums[i] == nums[i - 1]) {
                     continue;
                 }
             }
-            vector<int> temp;
-            while (right > left) {
-                if (nums[i] + nums[left] + nums[right] == 0) {
-                    temp.push_back(nums[i]);
-                    temp.push_back(nums[left]);
-                    temp.push_back(nums[right]);
-                    if (nums[left] == nums[left + 1]) {left++;}
-                    if (nums[right] == nums[right - 1]) {right--;}
-                    left++;
-                    right--;
-                    out.push_back(temp);
-                    temp.clear();
-                } else if (nums[i] + nums[left] + nums[right] < 0) {
-                    left++;
-                } else if (nums[i] + nums[left] + nums[right] > 0) {
-                    right--;
+
+            while (l < r) {
+
+                int s = nums[i] + nums[l] + nums[r];
+                if (s == 0) {
+                    out.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                }
+                else if(s < 0){
+                    l++;
+                }
+                else{
+                    r++;
                 }
             }
         }
