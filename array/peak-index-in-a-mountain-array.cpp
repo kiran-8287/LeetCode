c3edcb59811d1299d34f618f5f58e258fadd034e
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-09 00:45:48 +0530

    LC-peak-index-in-a-mountain-array [Runtime: N/A; Memory: N/A]

--- array/peak-index-in-a-mountain-array.cpp
+++ array/peak-index-in-a-mountain-array.cpp
@@ -3,16 +3,16 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int l = 0;
         int r = arr.size()-1;
-        while(l < r){
+        while(l <= r){
             int m = (l+r)/2;
             if(arr[m] > arr[m+1] && arr[m] > arr[m-1]){
                 return m;
             }
             else if(arr[m] > arr[m+1] && arr[m] < arr[m-1]){
-                r = m - 1;
+                r = m 
             }
             else if (arr[m] < arr[m+1] && arr[m] > arr[m-1]){
-                l = m + 1;
+                l = m
             }
         }
         return arr[l];
