bdfbe3702fb5dac27f8981e1feea42f745000123
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-09 00:44:26 +0530

    LC-peak-index-in-a-mountain-array [Runtime: N/A; Memory: N/A]

---
+++ binary-search/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l = 0;
+        int r = arr.size()-1;
+        while(l <= r){
+            int m = (l+r)/2;
+            if(arr[m] > arr[m+1] && arr[m] > arr[m-1]){
+                return m;
+            }
+            else if(arr[m] > arr[m+1] && arr[m] < arr[m-1]){
+                r = m - 1;
+            }
+            else if (arr[m] < arr[m+1] && arr[m] > arr[m-1]){
+                l = m + 1;
+            }
+        }
+        return arr[l];
+    }
+};
