71e60f4f1df0ec516add91a0b9dd634b3e9b846f
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-09 23:54:08 +0530

    LC-ones-and-zeroes [Runtime: N/A; Memory: N/A]

--- string/ones-and-zeroes.cpp
+++ string/ones-and-zeroes.cpp
@@ -19,25 +19,20 @@ public:
         int ones = 0;
         int out = 0;
         for(int i = 0; i < strs.size() - 1; i++){
-            bool isgood = true;
+            int count0 = 0;
+            int count1 = 0;
             for(int j = 0; j < strs[i].size(); j++){
                 if(strs[i][j] == '0'){
-                    if(zeros+1 > m){
-                        isgood = false;
-                        break;
-                    }
-                    zeros++;
+                    count0++;
                 }
                 else if(strs[i][j] == '1'){
-                    if(ones+1 > m){
-                        isgood = false;
-                        break;
-                    }
-                    ones++;
+                    count1++;
                 }
             }
-            if(isgood == true){
-                out++;
+            if(count1+ones <= n && count0+zeros <= m){
+                 count1 = count1+ones;
+                 count0 = count0+zeros;
+                 out++;
             }
             if(zeros == m && ones == n){
                 break; 
