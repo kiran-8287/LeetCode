970a6f16d83c22b89b6a70a469eed567f411e04a
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-10 00:08:29 +0530

    LC-sum-of-square-numbers [Runtime: N/A; Memory: N/A]

--- math/sum-of-square-numbers.cpp
+++ math/sum-of-square-numbers.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
+        if(c==1 || c==0){
+            return true;
+        }
         int l = 0;
         int r = c/2;
         while(l<=r){
