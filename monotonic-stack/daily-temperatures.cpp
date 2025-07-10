6c076471643496359c271c3440af610e52635f1e
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-10 16:15:19 +0530

    LC-daily-temperatures [Runtime: N/A; Memory: N/A]

--- monotonic-stack/daily-temperatures.cpp
+++ monotonic-stack/daily-temperatures.cpp
@@ -6,12 +6,12 @@ public:
             int temp = temperatures[i];
             int count = 0;
             for(int j = i; j < temperatures.size(); j++){
-                if(i != j && temperatures[j] < temp){
+                if(i != j && temperatures[j] <= temp){
                     if(j != temperatures.size()-1){
                         count++;
                     }
                 }
-                else if(i != j && temperatures[j] > temp){
+                else if(i != j && temperatures[j] >= temp){
                     count++;
                     break;
                 }
