3cf8ef1c3d81c9ac5e372977487a204b36b868ff
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-10 16:14:48 +0530

    LC-daily-temperatures [Runtime: N/A; Memory: N/A]

---
+++ stack/daily-temperatures.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> out;
+        for(int i = 0; i < temperatures.size(); i++){
+            int temp = temperatures[i];
+            int count = 0;
+            for(int j = i; j < temperatures.size(); j++){
+                if(i != j && temperatures[j] < temp){
+                    if(j != temperatures.size()-1){
+                        count++;
+                    }
+                }
+                else if(i != j && temperatures[j] > temp){
+                    count++;
+                    break;
+                }
+            }
+            out.push_back(count);
+        }
+        return out;
+    }
+};
