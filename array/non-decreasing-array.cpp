f2e5fbac715ce941d6a4d1d0921a08fd25b81fdd
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-11 02:02:18 +0530

    LC-non-decreasing-array [Runtime: N/A; Memory: N/A]

--- array/non-decreasing-array.cpp
+++ array/non-decreasing-array.cpp
@@ -9,9 +9,12 @@ public:
                     count++;
                 }
             }
-            else if(nums[i] > nums[i+1]){
-                nums[i] = nums[i+1];
-                count++;
+            else if(nums[i] > nums[i+1] ){
+                if(nums[i+1] > nums[0]){
+                    return false;
+                }
+                else{nums[i] = nums[i+1];
+                count++;}
             }      
         }
         if(count == 1){
