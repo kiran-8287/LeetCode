4a8eab984bafb55079b21bad7f3d56e99b69e881
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-10 21:01:36 +0530

    LC-single-element-in-a-sorted-array [Runtime: 0 ms; Memory: 26.3 MB]

--- array/single-element-in-a-sorted-array.cpp
+++ array/single-element-in-a-sorted-array.cpp
@@ -7,10 +7,10 @@ public:
         while(left <= right){
             int mid = (left+right)/2;
             
-            if(mid%2==0 && nums[mid] == nums[mid+1]){
+            if(mid%2==0 && mid+1 < nums.size() && nums[mid] == nums[mid+1]){
                 left = mid+1;
             }
-            else if(mid%2==1 && nums[mid] == nums[mid-1]){
+            else if(mid%2==1 && mid -1 >= 0 && nums[mid] == nums[mid-1]){
                 left = mid+1;
             }
             else{
