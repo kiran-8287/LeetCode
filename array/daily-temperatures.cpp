46835304e7f171267a838107b4092249485643e2
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-10 16:16:58 +0530

    LC-daily-temperatures [Runtime: N/A; Memory: N/A]

--- array/daily-temperatures.cpp
+++ array/daily-temperatures.cpp
@@ -5,17 +5,21 @@ public:
         for(int i = 0; i < temperatures.size(); i++){
             int temp = temperatures[i];
             int count = 0;
+            bool got = true;
             for(int j = i; j < temperatures.size(); j++){
                 if(i != j && temperatures[j] <= temp){
                     if(j != temperatures.size()-1){
                         count++;
+                        got = false;
                     }
                 }
                 else if(i != j && temperatures[j] >= temp){
                     count++;
+                    got = true;
                     break;
                 }
             }
+            if(got == false){count=0;}
             out.push_back(count);
         }
         return out;
