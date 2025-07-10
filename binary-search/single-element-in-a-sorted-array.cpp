efc680d53d3dbbdd45d1d18508bf1a46c3d5d4cf
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-10 20:55:16 +0530

    LC-single-element-in-a-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ binary-search/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right){
+            int mid = (left+right)/2;
+            
+            if(mid%2==0 && nums[mid] == nums[mid+1]){
+                left = mid+1;
+            }
+            else if(mid%2==1 && nums[mid] == nums[mid-1]){
+                left = mid+1;
+            }
+            else{
+                right = mid - 1;
+            }
+            
+        }
+        return nums[left];
+    }
+};
