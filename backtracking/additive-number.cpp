e86ee940a376976e210efbc2ab0db4b4fe976383
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-15 23:33:53 +0530

    LC-additive-number [Runtime: N/A; Memory: N/A]

--- backtracking/additive-number.cpp
+++ backtracking/additive-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool check(string &num,int i, bool &add){
-        if(i == num.length() - 2 ){
+        if(add == false){return false;}
+        if(i >= num.length() - 2 ){
             return true;
         }
-        if((num[i]-'0') + (num[i+1] - '0') == num[i+2] - '0'){
-            add = true;
+        if((num[i]-'0') + (num[i+1] - '0') != num[i+2] - '0'){
+            add = false;
         }
-        if(add == false){return false;}
         return check(num, i+1, add);
     }
     bool isAdditiveNumber(string num) {
