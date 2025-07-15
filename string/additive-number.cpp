89b69ddc8b94e9226e27f659f630c698b53e2148
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-15 23:29:35 +0530

    LC-additive-number [Runtime: N/A; Memory: N/A]

---
+++ string/additive-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool check(string &num,int i, bool &add){
+        if(i == num.length() - 2 ){
+            return true;
+        }
+        if((num[i]-'0') + (num[i+1] - '0') == num[i+2] - '0'){
+            add = true;
+        }
+        if(add == false){return false;}
+        return check(num, i+1, add);
+    }
+    bool isAdditiveNumber(string num) {
+        bool add = true;
+        return check(num, 0 , add);
+    }
+};
