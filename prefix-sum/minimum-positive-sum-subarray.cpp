f1a3356da89767df6322e04083880cfd0329afd6
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-18 02:11:04 +0530

    LC-minimum-positive-sum-subarray [Runtime: 4 ms; Memory: 29.5 MB]

--- prefix-sum/minimum-positive-sum-subarray.cpp
+++ prefix-sum/minimum-positive-sum-subarray.cpp
@@ -4,49 +4,34 @@ using namespace std;
 class Solution {
 public:
     int minimumSumSubarray(vector<int>& nums, int l, int r) {
-        int sum1 = 0;
-        for (int i = 0; i < l; i++) {
-            sum1 += nums[i];
-        }
-
-        int min1 = INT_MAX;
-        if (sum1 > 0) {
-            min1 = sum1;
-        }
-        int right = l;
-        int start = 0;
-        while (right < nums.size()) {
-            sum1 = sum1 - nums[start] + nums[right];
-            if (sum1 > 0) {
-                min1 = min(min1, sum1);
+        int out = INT_MAX;
+        for (int i = l; i <= r; i++) {
+            int sum = 0;
+            for (int j = 0; j < i; j++) {
+                sum += nums[j];
             }
-            right++;
-            start++;
-        }
-
-        int sum2 = 0;
-        int min2 = INT_MAX;
-        right = r;
-        start = 0;
-        for (int i = 0; i < r; i++) {
-            sum2 += nums[i];
-        }
-
-        if (sum2 > 0) {
-            min2 = sum2;
-        }
-        while (right < nums.size()) {
-            sum2 = sum2 - nums[start] + nums[right];
-            if (sum2 > 0) {
-                min2 = min(min2, sum2);
+            int mini = INT_MAX;
+            if (sum > 0) {
+                mini = sum;
             }
-            right++;
-            start++;
+            int right = i;
+            int start = 0;
+            while (right < nums.size()) {
+                sum = sum - nums[start] + nums[right];
+                if (sum > 0) {
+                    mini = min(mini, sum);
+                }
+                right++;
+                start++;
+            }
+            if(mini < out){
+                out = mini;
+            }
         }
 
-        if (min1 == INT_MAX && min2 == INT_MAX) {
+        if (out == INT_MAX) {
             return -1;
         }
-        return min(min1, min2);
+        return out;
     }
 };
