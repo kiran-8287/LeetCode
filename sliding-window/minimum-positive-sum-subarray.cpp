671038ded179f193b61f70c33985160bcc8484b2
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-18 01:58:16 +0530

    LC-minimum-positive-sum-subarray [Runtime: N/A; Memory: N/A]

--- sliding-window/minimum-positive-sum-subarray.cpp
+++ sliding-window/minimum-positive-sum-subarray.cpp
@@ -1,3 +1,6 @@
+#include <climits>
+using namespace std;
+
 class Solution {
 public:
     int minimumSumSubarray(vector<int>& nums, int l, int r) {
@@ -5,39 +8,45 @@ public:
         for (int i = 0; i < l; i++) {
             sum1 += nums[i];
         }
+
         int min1 = INT_MAX;
+        if (sum1 > 0) {
+            min1 = sum1;
+        }
         int right = l;
         int start = 0;
-        while (start < nums.size()-right) {
+        while (right < nums.size()) {
+            sum1 = sum1 - nums[start] + nums[right];
             if (sum1 > 0) {
                 min1 = min(min1, sum1);
             }
-            sum1 = sum1 - nums[start] + nums[right];
             right++;
             start++;
-            
         }
-        
+
         int sum2 = 0;
         int min2 = INT_MAX;
         right = r;
         start = 0;
-
         for (int i = 0; i < r; i++) {
             sum2 += nums[i];
         }
-        
-        while (start < nums.size()-right) {
+
+        if (sum2 > 0) {
+            min2 = sum2;
+        }
+        while (right < nums.size()) {
+            sum2 = sum2 - nums[start] + nums[right];
             if (sum2 > 0) {
                 min2 = min(min2, sum2);
             }
-            sum2 = sum2 - nums[start] + nums[right];
             right++;
             start++;
         }
-        if(min1 < 1000*100 || min2 < 1000*100){
-            return min(min1, min2);
+
+        if (min1 == INT_MAX && min2 == INT_MAX) {
+            return -1;
         }
-        return -1;
+        return min(min1, min2);
     }
 };
