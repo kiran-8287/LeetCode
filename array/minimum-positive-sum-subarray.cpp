0a3cfa028ee8e4d4e17795173481f4d66e0faf1d
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-18 01:43:27 +0530

    LC-minimum-positive-sum-subarray [Runtime: N/A; Memory: N/A]

--- array/minimum-positive-sum-subarray.cpp
+++ array/minimum-positive-sum-subarray.cpp
@@ -2,20 +2,12 @@ class Solution {
 public:
     int minimumSumSubarray(vector<int>& nums, int l, int r) {
         int sum1 = 0;
-        int sum2 = 0;
         for (int i = 0; i < l; i++) {
             sum1 += nums[i];
         }
-        for (int i = 0; i < r; i++) {
-            sum2 += nums[i];
-        }
-        int min1 = 1000*100;
-        int min2 = 1000*100;
+        int min1 = INT_MAX;
         if (sum1 > 0) {
                 min1 = sum1;
-        }
-        if (sum2 > 0) {
-                min2 = sum2;
         }
         int right = l;
         int start = 0;
@@ -27,8 +19,20 @@ public:
                 min1 = min(min1, sum1);
             }
         }
+        
+        int sum2 = 0;
+        int min2 = INT_MAX;
+        if (sum2 > 0) {
+                min2 = sum2;
+        }
+
         right = r;
         start = 0;
+
+        for (int i = 0; i < r; i++) {
+            sum2 += nums[i];
+        }
+        
         while (right < nums.size()) {
             sum2 = sum2 - nums[start] + nums[right];
             right++;
