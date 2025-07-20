56981aa4662ccc43f22444d105ae7081bbb9c1eb
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-21 01:02:14 +0530

    LC-minimum-number-of-arrows-to-burst-balloons [Runtime: 56 ms; Memory: 93.9 MB]

--- sorting/minimum-number-of-arrows-to-burst-balloons.cpp
+++ sorting/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(), points.end());
+        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+
         int i = 1;
         long long arrow = 1;
         int check = points[0][1];
         while(i < points.size()){
-            if(check >= points[i][0] && points[i][1] >= check){
+            if(check >= points[i][0] &&  check <= points[i][1]){
                 i++;
-                continue;
             }
             else{
                 check = points[i][1];
