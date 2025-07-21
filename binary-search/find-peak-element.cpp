9d8c415b6a00ed4b21a42c570dfee4eeb2cea3e6
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-21 23:51:58 +0530

    LC-find-peak-element [Runtime: 0 ms; Memory: 12.5 MB]

--- binary-search/find-peak-element.cpp
+++ binary-search/find-peak-element.cpp
@@ -3,21 +3,15 @@ public:
     int findPeakElement(vector<int>& nums) {
         int l = 0;
         int r = nums.size()-1;
-        while(l <= r){
+        while(l < r){
             int mid = (r+l)/2;
-            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]){
-                return mid;
-            }
-            else if(nums[mid] > nums[mid+1] && nums[mid] < nums[mid-1]){
-                r = mid-1;
-            }
-            else if(nums[mid] < nums[mid+1] && nums[mid] > nums[mid-1]){
-                l = mid + 1;
+            if(nums[mid] > nums[mid+1]){
+                r = mid;
             }
             else{
-                l++;
+                l = mid+1;
             }
         }
-        return nums[l];
+        return l;
     }
 };
