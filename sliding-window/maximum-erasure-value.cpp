d09ed379fb11b79f73d7ec834344c15bfe5e1ea9
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-23 00:45:03 +0530

    LC-maximum-erasure-value [Runtime: 53 ms; Memory: 103.2 MB]

--- sliding-window/maximum-erasure-value.cpp
+++ sliding-window/maximum-erasure-value.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-      int left = 0;
-      int right = 0;
-      unordered_map<int, int> map;
-      long long out = 0;
-      long long count = 0;
-      while(right < nums.size()){
-        if(map[nums[right]] == 0){
-            map[nums[right]]++;
-            count = count + nums[right];
-            out = max(out, count);
-        }
-        else{
-            while(left < right && map[nums[left]] != map[nums[right]]){
-                map[nums[left]] = 0;
-                count = count - nums[left];
-                left++;
+        int left = 0;
+        int right = 0;
+        unordered_map<int, int> map;
+        long long out = 0;
+        long long count = 0;
+        while (right < nums.size()) {
+            if (map[nums[right]] == 0) {
+                map[nums[right]]++;
+                count = count + nums[right];
+                out = max(out, count);
+            } else {
+                while (map[nums[right]] > 0) {
+                    map[nums[left]]--;
+                    count = count - nums[left];
+                    left++;
+                }
+                map[nums[right]]++;
+                count += nums[right];
+                out = max(out, count);
             }
-            left++;
-            out = max(out , count);
+            right++;
         }
-        right++;
-      }
-      return out; 
+        return out;
     }
 };
