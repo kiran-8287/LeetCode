8f24e1c87c97ec2bc9e967ada6d3ed0387053da8
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-24 01:29:01 +0530

    LC-number-of-burgers-with-no-waste-of-ingredients [Runtime: N/A; Memory: N/A]

---
+++ math/number-of-burgers-with-no-waste-of-ingredients.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
+        if(tomatoSlices%2 == 1){
+            return {};
+        }
+        if(cheeseSlices >= tomatoSlices){
+            return {};
+        }
+        int j = (tomatoSlices - 2*cheeseSlices)/2;
+        int s = (4*cheeseSlices - tomatoSlices)/2;
+        return {j,s};
+    }
+};
