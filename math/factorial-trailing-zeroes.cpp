8ca3fcfe9ba9b1697e969f26ecd33ca64954f784
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-07-31 23:43:01 +0530

    LC-factorial-trailing-zeroes [Runtime: 3 ms; Memory: 8.1 MB]

--- math/factorial-trailing-zeroes.cpp
+++ math/factorial-trailing-zeroes.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        bool goteven = false;
-        int count = 0;
-        for(int i = 1; i <= n; i++){
-            if(i % 10 == 0){
-                count++;
-            }
-            else{
-                if(i%2 == 0){
-                    goteven = true;
-                }
-                else{
-                    if(i%5 == 0 && goteven == true){
-                        count++;
-                        goteven = false;
-                    }
-                }
-            }
+        int out = 0;
+        int i = 1;
+        while(n/(pow(5,i)) > 0){
+            out = out + n/(pow(5,i));
+            i++;
         }
-        return count;
+        return out;
     }
 };
