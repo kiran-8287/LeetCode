7ec6804a038108682cfa5adbb71fe1fd8213a96f
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-08-01 22:36:54 +0530

    LC-swap-adjacent-in-lr-string [Runtime: N/A; Memory: N/A]

---
+++ string/swap-adjacent-in-lr-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canTransform(string start, string result) {
+        if(start.length() < 2){
+            return false;
+        }
+        int l = 0;
+        int r = 1;
+        while(r < start.length()){
+            if(start[l] == 'X' && start[r] == 'L'){
+                if(result[l] == 'X'){
+                    l++;
+                    r++;
+                    continue;
+                }
+                else if(result[l] == 'L' && result[r] == 'X'){
+                    l = l + 2;
+                    r = r + 2;
+                    continue;
+                }
+            }
+            else if(start[l] == 'R' && start[r] == 'X'){
+                if(result[l] == 'R'){
+                    l++;
+                    r++;
+                    continue;
+                }
+                else if(result[l] == 'X' && result[r] == 'R'){
+                    l = l + 2;
+                    r = r + 2;
+                    continue;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
