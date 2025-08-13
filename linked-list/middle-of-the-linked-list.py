6aa0bb37d2eb23f7f3884cfe7a0977aa879d6216
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-08-13 19:28:55 +0530

    LC-middle-of-the-linked-list [Runtime: 0 ms; Memory: 12.5 MB]

---
+++ linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if(head.next == None):
+            return head
+        elif(head.next.next == None):
+            return head.next
+        
+        slow = head
+        fast = head
+        while((fast.next != None) and (fast.next.next!=None)):
+            slow = slow.next
+            fast = fast.next.next
+        if(fast.next == None):
+            return slow
+        return slow.next
