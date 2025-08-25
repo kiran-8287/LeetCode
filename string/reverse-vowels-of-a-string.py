6318ba5f7d7818f1786121c8661913030b5bd8f4
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-08-25 21:14:05 +0530

    LC-reverse-vowels-of-a-string [Runtime: 16 ms; Memory: 13.6 MB]

--- string/reverse-vowels-of-a-string.py
+++ string/reverse-vowels-of-a-string.py
@@ -1,24 +1,17 @@
 class Solution(object):
     def reverseVowels(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        v = "aeiou"
+        vowels = "aeiouAEIOU"
         s = list(s)
-        left = 0
-        right = len(s)-1
-        while(left < right):
-            if((s[left] in v or s[left] in v.upper()) and (s[right] in v or s[right] in v.upper())):
-                s[left], s[right] = s[right], s[left]
-                right = right - 1
-                left = left + 1
-            elif((s[left] in v or s[left] in v.upper()) and (s[right] not in v and s[right] not in v.upper())):
-                right = right - 1
-            elif((s[left] not in v and s[left] not in v.upper()) and (s[right] in v or s[right] in v.upper())):
-                left = left + 1
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] not in vowels:
+                left += 1
+            elif s[right] not in vowels:
+                right -= 1
             else:
-                right = right - 1
-                left = left + 1
-        
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+
         return "".join(s)
