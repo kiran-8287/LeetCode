151900f28381fb4948fbab6158bfcdbd899bbb26
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-09-13 23:38:39 +0530

    LC-convert-sorted-array-to-binary-search-tree [Runtime: 2 ms; Memory: 23 MB]

--- binary-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ binary-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -1,25 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        if(nums.size() == 0){
-            return nullptr;
-        }
-        TreeNode* root = new TreeNode(nums[nums.size()/2]);
-        vector<int> l(nums.begin(), nums.begin() + nums.size()/2);
-        vector<int> r( nums.begin() + nums.size()/2+1,nums.end());
-        root->left = sortedArrayToBST(l);
-        root->right = sortedArrayToBST(r);
+        return build(nums, 0, nums.size() - 1);
+    }
+
+private:
+    TreeNode* build(vector<int>& nums, int left, int right) {
+        if (left > right) return nullptr;
+
+        int mid = left + (right - left) / 2;  // safe midpoint
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = build(nums, left, mid - 1);
+        root->right = build(nums, mid + 1, right);
+
         return root;
     }
 };
+
