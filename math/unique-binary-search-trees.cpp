db3a1bff30dd254a505edfa7518efa36c3954748
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-09-14 15:34:15 +0530

    LC-unique-binary-search-trees [Runtime: N/A; Memory: N/A]

--- math/unique-binary-search-trees.cpp
+++ math/unique-binary-search-trees.cpp
@@ -4,9 +4,9 @@ public:
         if(n==1){
             return 1;
         }
-        int num = 1;
-        int d1 = 1;
-        int d2 = 1;
+        long long num = 1;
+        long long d1 = 1;
+        long long d2 = 1;
 
         for(int i = 1; i < n+1; i++){
             d1 = d1 * i;
