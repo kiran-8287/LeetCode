14b1bea31cdc5f5740ea9da01899cce1d8d05ee9
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-09-19 19:03:52 +0530

    LC-balanced-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/balanced-binary-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int get_height(TreeNode* node){
+        if(node == nullptr){
+            return 0;
+        }
+        return 1 + max(get_height(node->left),get_height(node->right));
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root == nullptr){
+            return true;
+        }
+        int l = get_height(root->left);
+        int r = get_height(root->right);
+        if(abs(l-r) > 1){
+            return false;
+        }
+        return true;
+    }
+};
