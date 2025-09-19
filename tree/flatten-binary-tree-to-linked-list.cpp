f4e4a1d91257f36316bba57b42a6e2029eb26337
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-09-19 21:55:55 +0530

    LC-flatten-binary-tree-to-linked-list [Runtime: 0 ms; Memory: 17.5 MB]

--- tree/flatten-binary-tree-to-linked-list.cpp
+++ tree/flatten-binary-tree-to-linked-list.cpp
@@ -11,33 +11,16 @@
  */
 class Solution {
 public:
-    vector<int> preorder(TreeNode* root){
-        if(root == nullptr){
-            return {};
-        }
-        vector<int> result;
-        result.push_back(root->val);
-
-        vector<int> l = preorder(root->left);
-        vector<int> r = preorder(root->right);
-        result.insert(result.end(), l.begin(), l.end());
-        result.insert(result.end(), r.begin(), r.end());
-
-        return result;
-    }
+    TreeNode* prev = nullptr;
     void flatten(TreeNode* root) {
-        if(root != nullptr){
-            vector<int> list = preorder(root);
-            
-            TreeNode* head = new TreeNode(0);
-            TreeNode* curr = head;
-            for(int i = 0; i < list.size(); i++){
-                TreeNode* temp = new TreeNode(list[i]);
-                curr->right = temp;
-                curr = curr->right;
-            }
+        if(root!=nullptr){
+            flatten(root->right);
+            flatten(root->left);
+
+            root->right = prev;
             root->left = nullptr;
-            root->right = head->right->right;
+            prev = root;
+
         }
     }
 };
