82ebdd49db7e3cd69965abafbe2fa4a14836abb0
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-09-27 22:26:27 +0530

    LC-median-of-two-sorted-arrays [Runtime: N/A; Memory: N/A]

--- array/median-of-two-sorted-arrays.cpp
+++ array/median-of-two-sorted-arrays.cpp
@@ -5,18 +5,21 @@ public:
         int m = nums2.size();
         double med1 = 0;
         double med2 = 0;
-        if(n%2 == 0){
-            med1 = (nums1[n/2] + nums1[(n/2)-1])/2.0;
+        if(n != 0){
+            if(n%2 == 0){
+                med1 = (nums1[n/2] + nums1[(n/2)-1])/2.0;
+            }
+            else{
+                med1 = nums1[n/2];
+            }
         }
-        else{
-            med1 = nums1[n/2];
-        }
-        
-        if(m%2 == 0){
-            med2 = (nums2[m/2] + nums2[(m/2)-1])/2.0;
-        }
-        else{
-            med2 = nums2[m/2];
+        if(m!=0){
+            if(m%2 == 0){
+                med2 = (nums2[m/2] + nums2[(m/2)-1])/2.0;
+            }
+            else{
+                med2 = nums2[m/2];
+            }
         }
         return (med1+med2)/2.0;
     }
