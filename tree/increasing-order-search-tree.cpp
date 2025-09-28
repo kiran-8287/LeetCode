2a8e71944e169440570a7ca49ec23947bafbad40
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-09-28 23:32:45 +0530

    LC-increasing-order-search-tree [Runtime: 3 ms; Memory: 11.3 MB]

---
+++ tree/increasing-order-search-tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<TreeNode*>& nums){
+        if(root == NULL){
+            return;
+        }
+        inorder(root->left,nums);
+        nums.push_back(root);
+        inorder(root->right,nums);
+    }
+    TreeNode* increasingBST(TreeNode* root) {
+        vector<TreeNode*> nums;
+        inorder(root,nums);
+
+        TreeNode* new_root = nums[0];
+        TreeNode* temp = new_root;
+        for(int i = 1; i < nums.size(); i++){
+            temp->right = nums[i];
+            temp->left = NULL;
+            temp = temp->right;
+        }
+        temp->left = NULL;
+        temp->right = NULL;
+        return new_root;
+    }
+};
