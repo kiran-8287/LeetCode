49fd72f6b4e0abe029bb1406121129107be0d181
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2025-12-12 23:22:14 +0530

    LC-concatenation-of-array [Runtime: 0 ms; Memory: 16.9 MB]

--- array/concatenation-of-array.cpp
+++ array/concatenation-of-array.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> ans;
-        for(int i = 0; i < 2*nums.size(); i++){
-            if(i<nums.size()){ans.push_back(nums[i]);}
-            else if(i >= nums.size() && i < 2*nums.size()){ans.push_back(nums[i-nums.size()]);}
+        vector<int> ans(2*nums.size());
+        for(int i =0; i < 2*nums.size(); i++){
+            ans[i] = nums[i%nums.size()];
         }
         return ans;
     }
