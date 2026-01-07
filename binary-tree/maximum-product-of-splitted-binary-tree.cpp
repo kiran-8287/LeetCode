41cb9361ce2d4133fdf9c1d14cee39000f078478
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-01-07 21:28:22 +0530

    LC-maximum-product-of-splitted-binary-tree [Runtime: 0 ms; Memory: 82.5 MB]

--- binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -20,19 +20,13 @@ public:
             allsubtreesums.push_back(total);
             return total;
     }
-    vector<long long> subtreesum(TreeNode* root){
-        dfs(root);
-        return allsubtreesums;
-    }
     int maxProduct(TreeNode* root) {
-        if(root == nullptr) return 0;
-        long long total_sum = dfs(root);
-        vector<long long> sumlist = subtreesum(root);
-        long long out = 0;
-        for(int i = 0; i < sumlist.size(); i++){
-            int subtree_sum = sumlist[i];
-            out = max(out, (total_sum - subtree_sum) * subtree_sum);
+        allsubtreesums.clear();
+        long long totalSum = dfs(root);
+        long long ans = 0;
+        for (long long s : allsubtreesums) {
+            ans = max(ans, s * (totalSum - s));
         }
-        return out % 1000000007;
+        return ans % 1000000007;
     }
 };
