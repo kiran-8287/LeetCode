a1cb999effb304b577d9c8f51f6bbbdce29f4174
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-01-07 18:14:38 +0530

    LC-same-tree [Runtime: 0 ms; Memory: 12.8 MB]

--- breadth-first-search/same-tree.cpp
+++ breadth-first-search/same-tree.cpp
@@ -12,20 +12,26 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == nullptr && q == nullptr){
-            return true;
-        }
-        if((p == nullptr && q != nullptr )||(p != nullptr && q == nullptr)){
-            return false;
-        }
-        if(p->val != q->val){
-            return false;
-        }
-        if(!isSameTree(p->left, q->left)){
-            return false;
-        }
-        if(!isSameTree(p->right, q->right)){
-            return false;
+        vector<TreeNode*> st;
+        st.push_back(p);
+        st.push_back(q);
+        while(st.empty() == false){
+            TreeNode* node2 = st.back();
+            st.pop_back();
+            TreeNode* node1 = st.back();
+            st.pop_back();
+            if(node1 == nullptr && node2 == nullptr){
+                continue;
+            }
+            else if(node1 == nullptr || node2 == nullptr ||node1->val != node2->val){
+                return false;
+            }
+
+            st.push_back(node1->right);
+            st.push_back(node2->right);
+            st.push_back(node1->left);
+            st.push_back(node2->left);
+
         }
         return true;
     }
