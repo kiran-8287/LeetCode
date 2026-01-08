8ab84946a456f3a4574ac452b459d8847f9e9cf5
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-01-08 17:57:20 +0530

    LC-find-missing-and-repeated-values [Runtime: 0 ms; Memory: 25.9 MB]

--- array/find-missing-and-repeated-values.cpp
+++ array/find-missing-and-repeated-values.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        unordered_map<int,bool> map;
+        vector<bool> freq(grid.size()*grid.size()+1,false);
         vector<int> result;
-        for(int i = 1; i < grid.size()*grid.size()+1;i++){
-            map[i] = false;
-        }
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
-                if(map[grid[i][j]] == false){
-                    map[grid[i][j]] = true;
+                if(freq[grid[i][j]] == false){
+                    freq[grid[i][j]] = true;
                 }
-                else if(map[grid[i][j]] == true){
+                else if(freq[grid[i][j]] == true){
                     result.push_back(grid[i][j]);
                 }
             }
         }
         for(int i = 1; i < grid.size()*grid.size()+1;i++){
-            if(map[i] == false){
+            if(freq[i] == false){
                 result.push_back(i);
                 break;
             }
