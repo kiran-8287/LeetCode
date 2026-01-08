643a6cc59894c23915f77a745a091d295a87fb1d
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-01-08 18:39:57 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

---
+++ recursion/powx-n.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        else if(n < 0){
+            return (1/x)*myPow(x,n+1);
+        }
+        return x*myPow(x,n-1);
+    }
+};
