422739aa09faf15f857db14e2c3de2ff405b998c
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-02-03 22:31:52 +0530

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

---
+++ string/string-to-integer-atoi.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int sign = 1;
+        string temp = "";
+        for(int i =0; i < s.size(); i++){
+            if(s[i] == ' '){
+                if(temp == ""){
+                    continue;
+                }
+                break;
+            }
+            if(s[i]=='-' or s[i]=='+'){
+                if(i == 0 or (i > 0 and s[i-1] == ' ')){
+                    if(s[i]=='-'){sign = -1;}
+                    continue;
+                }
+                else{
+                    break;
+                }
+            }
+            if(!isdigit(s[i]) or s[i]==' '){
+                break;
+            }
+            else{
+                temp += s[i];
+            }
+        }
+        if(temp == ""){
+            return 0;
+        }
+        long long x = stoi(temp);
+        if(x > INT_MAX){
+            return INT_MAX;
+        }
+        return x*sign;
+    }
+};
