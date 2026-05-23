7ed9d217b6958c218f6ef24ca06d0c7083ddaf73
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-05-23 21:34:53 +0530

    LC-add-to-array-form-of-integer [Runtime: N/A; Memory: N/A]

---
+++ math/add-to-array-form-of-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        int temp = 0;
+        int n = num.size();
+        for(int i =0; i < n; i++){
+            temp += pow(10,n-i-1)*num[i];
+        }
+        int res = temp+k;
+        vector<int> ans;
+        while(res > 0){
+            ans.insert(ans.begin(),res%10);
+            res = res/10;
+        }
+        return ans;
+    }
+};
