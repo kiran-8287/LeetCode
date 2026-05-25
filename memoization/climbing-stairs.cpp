3da5bd557b029241a3da4b4015c79ae93e34b2dc
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-05-25 15:20:21 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- memoization/climbing-stairs.cpp
+++ memoization/climbing-stairs.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
-    unordered_map<int , int> map;
     int climbStairs(int n) {
-        if(n==0){
+        if(n == 1 or n == 0){
             return 1;
         }
-        if(n == 1 || n == 2){
-            return n;
-        }
-        if(map.find(n) != map.end()){
-            return map[n];
-        }
-        map[n] = climbStairs(n-1) + climbStairs(n-2);
-        return map[n];
+        return climbStairs(n-1)+climbStairs(n-2);
     }
 };
