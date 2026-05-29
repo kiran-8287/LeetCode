50df4b8ff1c8f10071a9722f6824e5df517c628c
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-05-29 21:25:31 +0530

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- stack/valid-parentheses.cpp
+++ stack/valid-parentheses.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> par;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('||s[i]=='{'||s[i]=='['){
-                par.push(s[i]);
-            }
-            else if(par.empty()){
-                return false;
-            }
-            
-            else if(s[i]==')'&& par.top()=='('){
-                par.pop();
-            }
-            else if(s[i]=='}'&& par.top()=='{'){
-                par.pop();
-            }
-            else if(s[i]==']'&& par.top()=='['){
-                par.pop();
+        stack<char> box;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                box.push(s[i]);
             }
             else{
-                return false;
+                if(box.empty() == true){
+                    return false;
+                }
+                else if((s[i] == ')' and box.top() != '(') ||
+                    (s[i] == ']' and box.top() != '[') ||
+                    (s[i] == '}' and box.top() != '{')){
+                    return false;
+                }
+                box.pop();
             }
         }
-        return par.empty();
+        return true;
     }
 };
