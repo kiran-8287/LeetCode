0edf3e5f94a930a6e915fbf705dafe526c8949ab
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-05-31 22:49:14 +0530

    LC-move-zeroes [Runtime: N/A; Memory: N/A]

--- array/move-zeroes.cpp
+++ array/move-zeroes.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int k = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != 0){
-                int temp = nums[i];
-                nums[i] = nums[k];
-                nums[k] = temp;
-                k++;
+        int i = 0;
+        int n = nums.size();
+        for(int j = 0; j < n; j++){
+            if(nums[j] != 0){
+                swap(nums[i],nums[j]);
+                i++;
             }
         }
     }
