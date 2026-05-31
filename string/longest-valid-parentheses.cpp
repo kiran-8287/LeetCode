da68b4a55f3d1d93603d57f71bc352aef16788f7
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-05-31 16:14:43 +0530

    LC-longest-valid-parentheses [Runtime: N/A; Memory: N/A]

--- string/longest-valid-parentheses.cpp
+++ string/longest-valid-parentheses.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        if(s == ""){
-            return 0;
-        }
-        stack<char> t;
-        int count = 0;
-        int temp = 0;
-        for(char c: s){
-            if(c == '('){
-                t.push(c);
+        int res = 0;
+        stack<int> t;
+        t.push(-1);
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '('){
+                t.push(i);
             }
             else{
-                if(t.empty() == false){
-                    t.pop();
-                    temp += 2;
-                    count = max(count,temp);
+                t.pop();
+                if(!t.empty()){
+                    res = max(res,i-t.top());
                 }
                 else{
-                    while(!t.empty()){
-                        t.pop();
-                    }
-                    temp = 0;
+                    t.push(i);
                 }
             }
         }
-        return count;
+        return res;
     }
 };
