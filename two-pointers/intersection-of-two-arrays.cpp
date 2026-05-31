5a1dd057b42c1c97b54a80baaa30c83dd1692ee3
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-05-31 23:03:13 +0530

    LC-intersection-of-two-arrays [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/intersection-of-two-arrays.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        unordered_set<int> s(nums1.begin(),nums1.end());
+        for(int n: nums2){
+            if(s.count(n)==1){
+                res.push_back(n);
+                s.erase(n);
+            }
+        }
+        return res;
+    }
+};
