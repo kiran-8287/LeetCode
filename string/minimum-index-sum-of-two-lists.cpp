ba267ff5aeae7001ce30c914fe9a829df15f9c8b
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-06-05 01:54:55 +0530

    LC-minimum-index-sum-of-two-lists [Runtime: N/A; Memory: N/A]

--- string/minimum-index-sum-of-two-lists.cpp
+++ string/minimum-index-sum-of-two-lists.cpp
@@ -8,7 +8,7 @@ public:
         int res = INT_MAX;
         vector<string> out;
         for(int i = 0; i < list2.size(); i++){
-            if(map[list2[i]] == -1){
+            if(map.find(list2[i]) != map.end()){
                 auto it = find(list1.begin(), list1.end(), list2[i]);
                 int j = it - list1.begin();
                 if(res >= i + j){
