cfc20715ace5e1fd8ca8b04896fcfcdb65adf8f4
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-06-05 00:27:22 +0530

    LC-crawler-log-folder [Runtime: N/A; Memory: N/A]

---
+++ stack/crawler-log-folder.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string> s;
+        for(string log: logs){
+            if(log == "./"){
+                continue;
+            }
+            else if(log == "../" && s.empty() == false){
+                s.pop();
+            }
+            else{
+                s.push(log);
+            }
+        }
+        return s.size();
+    }
+};
