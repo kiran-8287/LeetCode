7516559b85ded5bd0ea07cfe13f1bae93127e73e
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-06-05 01:51:24 +0530

    LC-minimum-index-sum-of-two-lists [Runtime: N/A; Memory: N/A]

---
+++ hash-table/minimum-index-sum-of-two-lists.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        unordered_map<string,int> map;
+        for(int i = 0; i < list1.size(); i++){
+            map[list1[i]] = -1;
+        }
+        int res = INT_MAX;
+        vector<string> out;
+        for(int i = 0; i < list2.size(); i++){
+            if(map[list2[i]] == -1){
+                auto it = find(list1.begin(), list1.end(), list2[i]);
+                int j = it - list1.begin();
+                if(res >= i + j){
+                    res = i + j;
+                    map[list2[i]] = i + j;
+                }
+            }
+        }
+        for(auto& p: map){
+            if(p.second == res){
+                out.push_back(p.first);
+            }
+        }
+        return out;
+    }
+};
