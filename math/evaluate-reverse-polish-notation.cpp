16ff88188ec6cb39ef8c932e687ab41a5690f070
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-16 16:27:56 +0530

    LC-evaluate-reverse-polish-notation [Runtime: N/A; Memory: N/A]

--- math/evaluate-reverse-polish-notation.cpp
+++ math/evaluate-reverse-polish-notation.cpp
@@ -2,19 +2,38 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> s;
-        for(int i = 0; i < tokens.size(); i++){
-            if(tokens[i] == "+"||tokens[i] == "-"||tokens[i] == "*"||tokens[i] == "/"){
-                int m = s.top();
+        int sum = 0;
+        for(string c: tokens){
+            if(c == "+"){
+                int a = (int)s.top();
                 s.pop();
-                int n = s.top();
+                int b = (int)s.top();
                 s.pop();
-                if(tokens[i] == "+"){s.push(n+m);}
-                else if(tokens[i] == "-"){s.push(n-m);}
-                else if(tokens[i] == "*"){s.push(n*m);}
-                else if(tokens[i] == "/"){s.push(n/m);}
-            }
+                s.push(a+b);}
+            if(c == "-"){
+                int a = (int)s.top();
+                s.pop();
+                int b = (int)s.top();
+                s.pop();
+                s.push(a+b);
+                s.push(a-b);}
+            if(c == "*"){
+                int a = (int)s.top();
+                s.pop();
+                int b = (int)s.top();
+                s.pop();
+                s.push(a+b);
+                s.push(a*b);}
+            if(c == "/"){
+                int a = (int)s.top();
+                s.pop();
+                int b = (int)s.top();
+                s.pop();
+                s.push(a+b);
+                s.push(a/b);}
             else{
-                s.push(stoi(tokens[i]));
+                int num = stoi(c);
+                s.push(num);
             }
         }
         return s.top();
