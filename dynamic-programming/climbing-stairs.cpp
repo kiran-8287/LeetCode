6fa82aad30b4affbf1453671e465832983ad4d7e
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-16 18:54:42 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- dynamic-programming/climbing-stairs.cpp
+++ dynamic-programming/climbing-stairs.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    unordered_map<int,int> map;
+    unordered_map<int, long long> memo;
     int climbStairs(int n) {
-        if(n==0 or n ==1){
-            return 1;
+        if(n <= 2){
+            return n;
         }
-        if(map[n] != 0){
-            return map[n];
-        }
-        else{
-            map[n] = climbStairs(n-1)+climbStairs(n-2);
-        }
-        return map[n];
+        if(memo.count(n)){return memo[n];}
+        return memo[n] = climbStairs(n-1)+climbStairs(n-2);
     }
 };
