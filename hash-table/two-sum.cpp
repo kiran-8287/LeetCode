a4e52193e6fa24255d84e422f6513f519a558fd0
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-19 22:27:47 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -4,15 +4,14 @@ public:
         vector<int> res;
         unordered_map<int, int> map;
         for(int i = 0; i < nums.size(); i++){
-            if(map[nums[i]] == 0){
-                map[nums[i]] = i;
-            }
-            else if(map.find(target-nums[i]))){
+            if(map.find(target-nums[i]) != map.end()){
                 res.push_back(i);
                 res.push_back(map[target-nums[i]]);
                 return res;
             }
-        }
+            else if(map[nums[i]] == 0){
+                map[nums[i]] = i;
+            }        }
         return res;
     }
 };
