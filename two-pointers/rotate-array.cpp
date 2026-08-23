b35d2941ca481f02af3fc95eb214ada0f34d3a5d
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-24 00:17:51 +0530

    LC-rotate-array [Runtime: N/A; Memory: N/A]

--- two-pointers/rotate-array.cpp
+++ two-pointers/rotate-array.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if(k == nums.size()){
-            return;
-        }
-        int left = 0;
-        int right = nums.size()-1-k;
+        k %= nums.size();
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin()+k);
         reverse(nums.begin()+k, nums.end());
