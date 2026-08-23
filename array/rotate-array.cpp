4498a12271f7861975b46aed9f011313306165d2
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-24 00:07:58 +0530

    LC-rotate-array [Runtime: N/A; Memory: N/A]

--- array/rotate-array.cpp
+++ array/rotate-array.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
-        vector<int> out(nums.size(), 0);
+        vector<int> res(nums.size(),0);
         for(int i = 0; i < nums.size(); i++){
-            out[(i+k)%nums.size()] = nums[i];
+            res[(i+k)%nums.size()] = nums[i];
         }
-        nums = out;
+        nums = res;
     }
 };
