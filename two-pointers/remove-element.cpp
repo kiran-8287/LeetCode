841ba37639eac5d00668f0ae069fe5c09d6400a2
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-24 15:50:19 +0530

    LC-remove-element [Runtime: N/A; Memory: N/A]

--- two-pointers/remove-element.cpp
+++ two-pointers/remove-element.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int count = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i]!=val){
-                nums[count] =nums[i];
-                count++;
+        int l = 0;
+        int r = 0;
+        while(r < nums.size()){
+            while(r < nums.size() && nums[r] == val){
+                r++;
             }
+            if(nums[l] != val){
+                r++;
+                l++;
+            }
+
+            else if(r < nums.size() && nums[l] == val){
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+            }
+            
         }
-        return count;
+        return l;
     }
 };
