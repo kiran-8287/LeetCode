db1924a3326782d47ae3c14e435593d4b0923a5e
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-31 01:34:01 +0530

    LC-removing-minimum-and-maximum-from-array [Runtime: N/A; Memory: N/A]

---
+++ array/removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+        int min_idx = 0;
+        int max_idx = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > maxi){
+                maxi = nums[i];
+                max_idx = i;
+            }
+            if(nums[i] < mini){
+                mini = nums[i];
+                min_idx = i;
+            }
+        }
+        if(maxi == mini){
+            return 1;
+        }
+        int l = min(min_idx, max_idx);
+        int r = max(min_idx,max_idx);
+        if(l+1+nums.size()-r < r+1){
+            return l+1+nums.size()-r;
+        }
+        return r+1;
+    }
+};
