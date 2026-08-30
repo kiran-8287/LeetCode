f46c7251dcb3c3384c492e85e858b6a9ac734918
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-31 01:42:45 +0530

    LC-removing-minimum-and-maximum-from-array [Runtime: N/A; Memory: N/A]

--- array/removing-minimum-and-maximum-from-array.cpp
+++ array/removing-minimum-and-maximum-from-array.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int maxi = INT_MIN;
-        int mini = INT_MAX;
+        int n = nums.size();
+
         int min_idx = 0;
         int max_idx = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] > maxi){
-                maxi = nums[i];
-                max_idx = i;
-            }
-            if(nums[i] < mini){
-                mini = nums[i];
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[min_idx])
                 min_idx = i;
-            }
+
+            if (nums[i] > nums[max_idx])
+                max_idx = i;
         }
-        if(maxi == mini){
-            return 1;
-        }
+
         int l = min(min_idx, max_idx);
-        int r = max(min_idx,max_idx);
-        if(l+1+nums.size()-r < r+1 && l+1+nums.size()-r < r-l+1){
-            return l+1+nums.size()-r;
-        }
-        else{
-            if(r+1 < r-l+1){
-                return r+1;
-            }
-            return r-l+1;
-        }
-        return 1;
+        int r = max(min_idx, max_idx);
+
+        int left = r + 1;
+        int right = n - l;             
+        int both = l + 1 + n - r;      
+
+        return min({left, right, both});
     }
 };
