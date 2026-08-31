98c983b2d629ed8aec9fe169f9c229e727e07de3
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-31 14:21:22 +0530

    LC-rotate-list [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/rotate-list.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(k==0 || head == nullptr || head->next == nullptr){
+            return head;
+        }
+        int n = 0;
+        ListNode* temp = head;
+        while(temp != nullptr){
+            temp = temp->next;
+            n++;
+        }
+        k = k%n;
+        ListNode* after = head;
+        ListNode* before = nullptr;
+        ListNode* temp1 = after;
+        for(int i = 0; i < n-k-1; i++){
+            temp1 = temp1->next;
+        }
+        before = temp1->next;
+        temp1->next = nullptr;
+        temp = before;
+        while(temp->next != nullptr){
+            temp = temp->next;
+        }
+        temp->next = after;
+        return before;
+    }
+};
