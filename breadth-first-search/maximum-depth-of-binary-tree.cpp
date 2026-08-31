552491233e1d7095bf7f6dfe5f60897e4fdcdc82
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-31 15:03:34 +0530

    LC-maximum-depth-of-binary-tree [Runtime: N/A; Memory: N/A]

--- breadth-first-search/maximum-depth-of-binary-tree.cpp
+++ breadth-first-search/maximum-depth-of-binary-tree.cpp
@@ -15,6 +15,6 @@ public:
         if(root == nullptr){
             return 0;
         }
-        return max(maxDepth(root->left),maxDepth(root->right))+1;
+        return 1+ max(maxDepth(root->left),maxDepth(root->right));
     }
 };
