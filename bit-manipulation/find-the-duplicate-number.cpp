9354d004f288538fe99b67a74d36f4c6fb4ea98d
Sai Kiran Vullengala <142401034@smail.iitpkd.ac.in>
2026-08-31 12:37:51 +0530

    LC-find-the-duplicate-number [Runtime: N/A; Memory: N/A]

--- bit-manipulation/find-the-duplicate-number.cpp
+++ bit-manipulation/find-the-duplicate-number.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int, int> map;
-        int out = nums[0];
-        for(int i = 0; i < nums.size(); i++){
-            if(map[nums[i]] == 0){
-                map[nums[i]] = 1;
-            }
-            else{
-                out = nums[i];
-            }
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return out;
+
+        return slow;
     }
 };
